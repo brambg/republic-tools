097400e921c5b6192160df0693ec7e27b45549d5
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-01-19 13:58:00 +0100

    add id argument to <l>

--- scripts/rt-create-pos.py
+++ scripts/rt-create-pos.py
@@ -71,17 +71,27 @@ def main():
     with open(annotation_store_path) as f:
         annotations = json.load(f)
     print(f" {len(annotations)} annotations read.")
+    line_annotations = [a for a in annotations if a["label"] == "lines"]
+    line_ids = {}
+    for la in line_annotations:
+        line_idx = la["begin_anchor"]
+        line_id = la["id"]
+        line_ids[line_idx] = line_id
 
     print(f"- reading {text_store_path} ...", end="", flush=True)
     with open(text_store_path) as f:
         text_store = json.load(f)
         lines = text_store['_resources'][0]['_ordered_segments']
     print(f" {len(lines)} lines read.")
+    fixed_lines = [l.replace("\n", " ") for l in lines]
 
-    fixed_lines = [add_newline(l) for l in lines]
-
-    selection = fixed_lines
+    selection = fixed_lines  # [0:1000]
     # ic(selection)
+    line_idx_for_ending_offset = {}
+    offset = 0
+    for i, line in enumerate([l for l in selection if l != "\n"]):
+        offset += len(line) + 1
+        line_idx_for_ending_offset[offset] = i
     text = ' '.join(selection)
     nlp.max_length = len(text)
     print(f"- spacy: processing {nlp.max_length} chars ...", end="", flush=True)
@@ -97,44 +107,61 @@ def main():
     for sentence in doc.sents:
         sentence_start = token_index
         for token in sentence:
-            x = token.text_with_ws.replace("\n", "§")
-            # ic(x)
             word = token.text.strip().strip('#')
             if word:
-                lemma = token.lemma_.strip()
-                if not lemma:
-                    ic(word)
-                    lemma = word
-                    ic(lemma)
-                pos = token.pos_
-                if not pos:
-                    raise Exception(
-                        f"lemma ({lemma}) or pos ({pos}) empty for word ({word}) after {input_doc.tokens[-3].word} {input_doc.tokens[-2].word} {input_doc.tokens[-1].word}")
-                pos_token = POSToken(word=word, lemma=lemma, pos=pos)
-
+                pos_token = to_pos_token(input_doc, token, word)
                 input_doc.tokens.append(pos_token)
+                token_index += 1
 
-                if token.text_with_ws == "\n ":  # last token of line
-                    line = " ".join([t.word for t in input_doc.tokens[line_start:token_index]])
-                    input_doc.spans.append(
-                        Span("l", line_start, token_index, parameters={"anchor": anchor_number, "text": line}))
-                    # ic(line)
-                    anchor_number += 1
-                    line_start = token_index
+            token_end_offset = token.idx + len(token.text_with_ws)
+            token_is_last_of_line = token_end_offset in line_idx_for_ending_offset
+            if token_is_last_of_line:  # last token of line
+                line = " ".join([t.word for t in input_doc.tokens[line_start:token_index]])
+                line_idx = line_idx_for_ending_offset[token_end_offset]
+                input_doc.spans.append(
+                    Span("l", line_start, token_index,
+                         parameters={
+                             "id": line_ids[line_idx],
+                             # "line_idx": line_idx,
+                             # "anchor_number": anchor_number,
+                             # "text": line
+                         }))
+
+                # ic(line)
+                # x = f"{line_ids[line_idx]} {line}"
+                # ic(x)
+                anchor_number += 1
+                line_start = token_index
 
                 # if token.text_with_ws == "\n":  # page ends
                 #     input_doc.spans.append(Span("p", page_start, token_index - 1))
                 #     page_start = token_index
 
-                token_index += 1
-
-        sentence_end = token_index
-        if sentence_end > sentence_start:
-            input_doc.spans.append(Span("s", sentence_start, sentence_end))
-            # sent = " ".join([t.word for t in input_doc.tokens[sentence_start:sentence_end]])
-            # ic(sent)
+        add_sentence_span(input_doc, sentence_start, token_index)
     # ic(input_doc)
     export(input_doc, "out/input.cif")
+
+
+def add_sentence_span(input_doc, sentence_start, token_index):
+    sentence_end = token_index
+    if sentence_end > sentence_start:
+        input_doc.spans.append(Span("s", sentence_start, sentence_end))
+        # sent = " ".join([t.word for t in input_doc.tokens[sentence_start:sentence_end]])
+        # ic(sent)
+
+
+def to_pos_token(input_doc, token, word):
+    lemma = token.lemma_.strip()
+    if not lemma:
+        ic(word)
+        lemma = word
+        ic(lemma)
+    pos = token.pos_
+    if not pos:
+        raise Exception(
+            f"lemma ({lemma}) or pos ({pos}) empty for word ({word}) after {input_doc.tokens[-3].word} {input_doc.tokens[-2].word} {input_doc.tokens[-1].word}")
+    pos_token = POSToken(word=word, lemma=lemma, pos=pos)
+    return pos_token
 
 
 if __name__ == '__main__':
