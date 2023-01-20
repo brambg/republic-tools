ca378fc53d0d795f003dadf243d8aca1761a7323
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-01-20 17:55:41 +0100

    refactorings, add resolution, attendance_list, page spans

--- scripts/rt-create-pos.py
+++ scripts/rt-create-pos.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass, field
 from typing import Dict, List, Any
 
 import spacy as spacy
-from icecream import ic
 from loguru import logger
 
 spacy_core = "nl_core_news_lg"
@@ -46,38 +45,19 @@ class ComplexHandler(json.JSONEncoder):
 def main():
     nlp = spacy.load(spacy_core)
 
-    logger.info(f"- reading {annotation_store_path} ...", end="", flush=True)
-    with open(annotation_store_path) as f:
-        annotations = json.load(f)
-    logger.info(f" {len(annotations)} annotations read.")
-    line_annotations = [a for a in annotations if a["type"] == "line"]
-    line_ids = {}
-    for la in line_annotations:
-        line_idx = la["begin_anchor"]
-        line_id = la["id"]
-        line_ids[line_idx] = line_id
+    annotations, line_ids = load_annotations()
 
-    logger.info(f"- reading {text_store_path} ...", end="", flush=True)
-    with open(text_store_path) as f:
-        text_store = json.load(f)
-        lines = text_store['_resources'][0]['_ordered_segments']
-    logger.info(f" {len(lines)} lines read.")
+    lines = load_text_lines()
     fixed_lines = [l.replace("\n", " ") for l in lines]
 
     selection = fixed_lines  # [0:1000]
     # ic(selection)
-    line_idx_for_ending_offset = {}
-    offset = 0
-    for i, line in enumerate([l for l in selection if l != "\n"]):
-        offset += len(line) + 1
-        line_idx_for_ending_offset[offset] = i
-    text = ' '.join(selection)
-    nlp.max_length = len(text)
-    logger.info(f"- spacy: processing {nlp.max_length} chars ...", end="", flush=True)
-    doc = nlp(text)
+    line_idx_for_ending_offset = index_line_ending_offset(selection)
 
-    input_doc = BlackLabInputDocument()
-    input_doc.metadata["title"] = "republic-1728"
+    doc = calculate_pos(nlp, selection)
+
+    input_doc = init_input_doc()
+
     token_index = 0
     line_start = token_index
     anchor_number = 0
@@ -119,11 +99,78 @@ def main():
 
         add_sentence_span(input_doc, sentence_start, token_index)
 
-    add_attendant_spans(annotations, input_doc, token_spans_per_anchor)
-    add_session_spans(annotations, input_doc, token_spans_per_anchor)
+    add_annotation_spans(input_doc, annotations, token_spans_per_anchor)
     # ic(input_doc)
     export(input_doc, "out/input.cif")
     export_json(input_doc)
+
+
+def init_input_doc():
+    input_doc = BlackLabInputDocument()
+    input_doc.metadata["title"] = "republic-1728"
+    input_doc.metadata["text_source"] = text_store_path
+    input_doc.metadata["annotation_source"] = annotation_store_path
+    return input_doc
+
+
+def add_annotation_spans(input_doc, annotations, token_spans_per_anchor):
+    add_spans(annotations=annotations, input_doc=input_doc, token_spans_per_anchor=token_spans_per_anchor,
+              annotation_type="attendant", span_tag="attendant", parameter_func=lambda a: a["metadata"])
+
+    add_spans(annotations=annotations, input_doc=input_doc, token_spans_per_anchor=token_spans_per_anchor,
+              annotation_type="session", span_tag="session", parameter_func=session_parameters)
+
+    add_spans(annotations=annotations, input_doc=input_doc, token_spans_per_anchor=token_spans_per_anchor,
+              annotation_type="republic_paragraph", span_tag="paragraph", parameter_func=paragraph_parameters)
+
+    add_spans(annotations=annotations, input_doc=input_doc, token_spans_per_anchor=token_spans_per_anchor,
+              annotation_type="attendance_list", span_tag="attendance_list", parameter_func=attendance_list_parameters)
+
+    add_spans(annotations=annotations, input_doc=input_doc, token_spans_per_anchor=token_spans_per_anchor,
+              annotation_type="page", span_tag="page", parameter_func=lambda a: {"scan_id": a["metadata"]["scan_id"]})
+
+    add_spans(annotations=annotations, input_doc=input_doc, token_spans_per_anchor=token_spans_per_anchor,
+              annotation_type="resolution", span_tag="resolution", parameter_func=resolution_parameters)
+
+
+def calculate_pos(nlp, selection):
+    text = ' '.join(selection)
+    nlp.max_length = len(text)
+    logger.info(f"spacy: processing {nlp.max_length} chars ...")
+    doc = nlp(text)
+    return doc
+
+
+def index_line_ending_offset(selection):
+    line_idx_for_ending_offset = {}
+    offset = 0
+    for i, line in enumerate([l for l in selection if l != "\n"]):
+        offset += len(line) + 1
+        line_idx_for_ending_offset[offset] = i
+    return line_idx_for_ending_offset
+
+
+def load_text_lines():
+    logger.info(f"reading {text_store_path} ...")
+    with open(text_store_path) as f:
+        text_store = json.load(f)
+        lines = text_store['_resources'][0]['_ordered_segments']
+    logger.info(f" {len(lines)} lines read.")
+    return lines
+
+
+def load_annotations():
+    logger.info(f"reading {annotation_store_path} ...")
+    with open(annotation_store_path) as f:
+        annotations = json.load(f)
+    logger.info(f" {len(annotations)} annotations read.")
+    line_annotations = [a for a in annotations if a["type"] == "line"]
+    line_ids = {}
+    for la in line_annotations:
+        line_idx = la["begin_anchor"]
+        line_id = la["id"]
+        line_ids[line_idx] = line_id
+    return annotations, line_ids
 
 
 def export_json(input_doc):
@@ -134,7 +181,7 @@ def export_json(input_doc):
 
 
 def export(input_doc, path):
-    logger.info(f"- exporting to {path} ...", end="", flush=True)
+    logger.info(f"exporting to {path} ...")
     with open(path, "w") as f:
         f.write("DOC_START\n")
         for k, v in input_doc.metadata.items():
@@ -168,38 +215,54 @@ def add_newline(l: str) -> str:
     return l if l.endswith("\n") else f"{l}\n"
 
 
-def add_attendant_spans(annotations, input_doc, token_spans_per_anchor):
-    attendant_annotations = [a for a in annotations if a["type"] == "attendant"]
-    for aa in attendant_annotations:
-        a_id = aa["id"]
-        begin_anchor = aa["begin_anchor"]
-        end_anchor = aa["end_anchor"]
+def add_spans(annotations, input_doc, token_spans_per_anchor, annotation_type, span_tag, parameter_func):
+    attendant_annotations = [a for a in annotations if a["type"] == annotation_type]
+    logger.info(f"{len(annotations)} {annotation_type} annotations found")
+    for annotation in attendant_annotations:
+        annotation_id = annotation["id"]
+        begin_anchor = annotation["begin_anchor"]
+        end_anchor = annotation["end_anchor"]
         if begin_anchor in token_spans_per_anchor and end_anchor in token_spans_per_anchor:
             start_token = token_spans_per_anchor[begin_anchor][0]
             end_token = token_spans_per_anchor[end_anchor][1]
-            metadata = aa["metadata"]
-            metadata["id"] = a_id
+            parameters = parameter_func(annotation)
+            parameters["id"] = annotation_id
             input_doc.spans.append(
-                Span("attendant", start_token, end_token,
-                     parameters=metadata))
-
-
-def add_session_spans(annotations, input_doc, token_spans_per_anchor):
-    session_annotations = [a for a in annotations if a["type"] == "session"]
-    for aa in session_annotations:
-        begin_anchor = aa["begin_anchor"]
-        end_anchor = aa["end_anchor"]
-        if begin_anchor in token_spans_per_anchor and end_anchor in token_spans_per_anchor:
-            start_token = token_spans_per_anchor[begin_anchor][0]
+                Span(span_tag, start_token, end_token, parameters=parameters))
+        else:
+            if begin_anchor not in token_spans_per_anchor:
+                logger.warning(f"begin_anchor {begin_anchor} not a key"
+                               f" in token_spans_per_anchor, no {span_tag} span added!")
             if end_anchor not in token_spans_per_anchor:
-                ic(aa, token_spans_per_anchor)
-            end_token = token_spans_per_anchor[end_anchor][1]
-            parameters = aa["metadata"]
-            parameters.pop("resolution_ids")
-            parameters.pop("text_page_num")
-            input_doc.spans.append(
-                Span("session", start_token, end_token,
-                     parameters=parameters))
+                logger.warning(f"end_anchor {end_anchor} not a key"
+                               f" in token_spans_per_anchor, no {span_tag} span added!")
+
+
+def session_parameters(annotation: Dict[str, Any]) -> Dict[str, Any]:
+    parameters = annotation["metadata"]
+    parameters.pop("resolution_ids")
+    parameters.pop("text_page_num")
+    return parameters
+
+
+def attendance_list_parameters(annotation: Dict[str, Any]) -> Dict[str, Any]:
+    parameters = annotation["metadata"]
+    parameters.pop("text_page_num")
+    return parameters
+
+
+def resolution_parameters(annotation: Dict[str, Any]) -> Dict[str, Any]:
+    parameters = annotation["metadata"]
+    parameters.pop("text_page_num")
+    parameters.pop("proposition_origin")
+    return parameters
+
+
+def paragraph_parameters(annotation: Dict[str, Any]) -> Dict[str, Any]:
+    parameters = annotation["metadata"]
+    parameters.pop("text_page_num")
+    parameters.pop("page_num")
+    return parameters
 
 
 def add_sentence_span(input_doc, sentence_start, token_index):
@@ -213,9 +276,8 @@ def add_sentence_span(input_doc, sentence_start, token_index):
 def to_pos_token(input_doc, token, word):
     lemma = token.lemma_.strip()
     if not lemma:
-        ic(word)
+        logger.warning(f"no lemma found, using {word}")
         lemma = word
-        ic(lemma)
     pos = token.pos_
     if not pos:
         raise Exception(
